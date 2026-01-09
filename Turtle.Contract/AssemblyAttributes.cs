be233df45ff088c4d238716b9b47e2a115e96310
gubard <maksimovsergey08@gmail.com>
2026-01-09 23:37:22 +0200

    Updated `SqliteAdo` attribute in `AssemblyAttributes.cs` to include the optional flag parameter.

--- Turtle.Contract/AssemblyAttributes.cs
+++ Turtle.Contract/AssemblyAttributes.cs
@@ -1,5 +1,5 @@
 ﻿using Nestor.Db.Models;
 using Turtle.Contract.Models;
 
-[assembly: SqliteAdo(typeof(CredentialEntity), nameof(CredentialEntity.Id))]
+[assembly: SqliteAdo(typeof(CredentialEntity), nameof(CredentialEntity.Id), false)]
 [assembly: SourceEntity(typeof(CredentialEntity), nameof(CredentialEntity.Id))]
