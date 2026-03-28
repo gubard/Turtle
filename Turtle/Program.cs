71136ec106934d82e2a30d010fcb75ceaa5f02e7
gubard <maksimovsergey08@gmail.com>
2026-03-28 20:21:44 +0200

    Switch `CredentialAdoDbService` to `CredentialLiteDbService` in Zeus app creation

--- Turtle/Program.cs
+++ Turtle/Program.cs
@@ -32,7 +32,7 @@ await WebApplication
     .CreateBuilder(args)
     .CreateAndRunZeusApp<
         ICredentialService,
-        CredentialAdoDbService,
+        CredentialLiteDbService,
         TurtleGetRequest,
         TurtlePostRequest,
         TurtleGetResponse,
